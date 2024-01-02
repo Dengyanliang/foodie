d50eac5125fce69b1e78b3d53388be9c89bf80a7
dengyanliang <457718426@qq.com>
2024-01-02 15:26:06 +0800

    foodie项目初始化

---
+++ foodie-api/src/main/java/com/deng/Application.java
@@ -0,0 +1,16 @@
+package com.deng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Desc:
+ * @Auther: dengyanliang
+ * @Date: 2023/12/15 18:09
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
---
+++ foodie-api/src/main/java/com/deng/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.deng.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Desc:
+ * @Auther: dengyanliang
+ * @Date: 2023/12/15 18:12
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public Object hello(){
+        return "hello world~";
+    }
+
+}
